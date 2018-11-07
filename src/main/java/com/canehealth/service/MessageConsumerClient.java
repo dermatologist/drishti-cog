9228e8dab432dd9346dc6a4e93e26a2a132ccbdc
Bell Eapen <github@gulfdoctor.net>
2018-11-07 12:24:46 -0500

    Oauth progress in MessageConsumerClient.

--- src/main/java/com/canehealth/service/MessageConsumerClient.java
+++ src/main/java/com/canehealth/service/MessageConsumerClient.java
@@ -8,6 +8,7 @@ import org.apache.camel.impl.DefaultCamelContext;
 import org.apache.camel.model.dataformat.JsonLibrary;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import java.util.Date;
 
@@ -16,21 +17,23 @@ public class MessageConsumerClient {
 
     private static final Logger LOG = LoggerFactory.getLogger(MessageConsumerClient.class);
 
-    private Date from;
-    private Date to;
+    @Value("${app.drishti.pollinterval}")
+    private String pollInterval;
+
     CamelContext camelContext = new DefaultCamelContext();
 
     RouteBuilder routeBuilder = new RouteBuilder() {
 
         @Override
         public void configure() {
-            from("timer://scheduler?period=30s")
+            from("timer://scheduler?period="+pollInterval+"s")
                     .log("get access token")
                     .to("direct:authService");
 
             from("direct:authService").tracing()
                     .setHeader(Exchange.HTTP_PATH)
-                    .simple("<auth service context>/oauth2/token")
+//                    .simple("<auth service context>/oauth2/token")
+                    .simple("omh/oauth2/token")
                     .setHeader("CamelHttpMethod")
                     .simple("POST")
                     .setHeader("Content-Type")
@@ -39,18 +42,21 @@ public class MessageConsumerClient {
                     .simple("application/json")
                     .setBody()
                     .constant("grant_type=client_credentials&client_id=<client id>&client_secret=<client sec>")
-                    .to("https4://<remote auth service url>")
+//                    .to("https4://<remote auth service url>")
+                    .to("http4://localhost/oauth2/token")
+
                     .convertBodyTo(String.class)
                     .log("response from API: " + body())
                     .choice()
                     .when().simple("${header.CamelHttpResponseCode} == 200")
-                    .unmarshal().json(JsonLibrary.Jackson, OAuth2AccessToken.class)
+//                    .unmarshal().json(JsonLibrary.Jackson, OAuth2AccessToken.class)
                     .setHeader("jwt").simple("${body.access_token}")
-                    .to("direct:<some direct route>")
+//                    .to("direct:<some direct route>")
+                    .to("direct:processToken")
                     .otherwise()
                     .log("Not Authenticated!!!");
 
-            from("direct:<some direct route>").tracing()
+            from("direct:processToken").tracing()
                     .log("body: " + body().toString())
                     .setBody().constant(null)
                     .setHeader(Exchange.HTTP_PATH)
@@ -61,7 +67,8 @@ public class MessageConsumerClient {
                     .simple("application/json")
                     .setHeader("Authorization")
                     .simple("${header.jwt}")     // <<<<<< HERE YOU GET YOUR AUTH TOKEN GRANTED IN PREVIOUS ROUTE >>>>>>
-                    .to("https4://<remote secured service url>")
+//                    .to("https4://<remote secured service url>")
+                    .to("http4://localhost/omh/auth")
                     .convertBodyTo(String.class)
                     .choice()
                     .when().simple("${header.CamelHttpResponseCode} == 200")
@@ -71,7 +78,8 @@ public class MessageConsumerClient {
                     + " result = canais[idx].id;"
                     + "")
                     .log("response from globosat API: " + body())
-                    .to("direct:<another route>")
+//                    .to("direct:<another route>")
+                    .to("http4://localhost/omh/dsu")
                     .otherwise()
                     .log("Error!!!");
         }
@@ -80,8 +88,7 @@ public class MessageConsumerClient {
 
     private MessageConsumerClient() throws Exception {
         this.camelContext.addRoutes(this.routeBuilder);
-//        this.from = from;
-//        this.to = to;
+
     }
 
     public void consume() {
@@ -92,19 +99,4 @@ public class MessageConsumerClient {
         }
     }
 
-    public Date getFrom() {
-        return from;
-    }
-
-    public void setFrom(Date from) {
-        this.from = from;
-    }
-
-    public Date getTo() {
-        return to;
-    }
-
-    public void setTo(Date to) {
-        this.to = to;
-    }
 }
