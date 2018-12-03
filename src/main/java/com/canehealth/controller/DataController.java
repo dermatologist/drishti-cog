bea23ffa2bbe43d35f8339001b01b92f09bd5b40
dermatologist <github@gulfdoctor.net>
2018-12-03 18:53:13 -0500

    ProcessBundle changed to post bundle.

--- src/main/java/com/canehealth/controller/DataController.java
+++ src/main/java/com/canehealth/controller/DataController.java
@@ -1,8 +1,6 @@
 package com.canehealth.controller;
 
-import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.model.primitive.IdDt;
-import ca.uhn.fhir.parser.IParser;
 import ca.uhn.fhir.rest.api.MethodOutcome;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
 import com.canehealth.config.DataConfig;
@@ -15,8 +13,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
-
 @RestController
 public class DataController {
     private final Logger logger = LoggerFactory.getLogger(DataController.class);
@@ -24,14 +20,14 @@ public class DataController {
     private DataConfig dataConfig;
     private IGenericClient fhirClient;
     //private static FhirContext fhirContext;
-    private final FhirContext ctx;
+    //private final FhirContext ctx;
 
     public DataController(DatasetInitializer dataInitializer, IGenericClient fhirClient, DataConfig dataConfig) {
         this.dataConfig = dataConfig;
         this.fhirClient = fhirClient;
         this.dataInitializer = dataInitializer;
         //fhirContext = FhirContext.forDstu3();
-        ctx = FhirContext.forDstu3();
+        //ctx = FhirContext.forDstu3();
     }
 
     /**
@@ -40,10 +36,8 @@ public class DataController {
      */
     //GET https://apps.hdap.gatech.edu/hapiR4/baseR4/DocumentReference?subject=EXxcda
     @PostMapping("/ProcessBundle")
-    public ResponseEntity processBundle(@RequestBody Map<String, String> json) {
-        String uuid = json.get("uuid");
-        final IParser parser = ctx.newJsonParser();
-        Bundle bundle = parser.parseResource(Bundle.class, json.get("bundle"));
+    public ResponseEntity processBundle(@RequestBody Bundle bundle) {
+        String uuid = bundle.getId();
         logger.info("Processing bundle: ", uuid);
 
         // First resource in the bundle is the patient
